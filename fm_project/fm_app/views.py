7d6216e0bba5ae54100797617b41651c0af38201
Rohan Rajesh P <rohan.rajesh@kaleyra.com>
2022-07-27 23:41:01 +0530

    Created URL, and View for the trials. Added fm_app in settings.py. Created service layer for processing data

--- fm_project/fm_app/views.py
+++ fm_project/fm_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views import View
+from django.http.response import JsonResponse
 
-# Create your views here.
+from service import TrialService
+
+
+class Trial(View):
+    """
+    In this class, we will receive the data from the FE, process it,
+    and send back the data of the next trial. If it is the first trial (T1),
+    then we won't be receiving any data
+    """
+
+    def post(self, request):
+        data = request.POST
+        trial_service = TrialService()
+        if not data:
+            return JsonResponse(trial_service.get_trial_data(), status=200)
+        trial_no = request.POST.get("trial_no")
+        return JsonResponse(trial_service.get_trial_data(trial_no), status=200)
---
+++ fm_project/service/trial_service.py
@@ -0,0 +1,17 @@
+"""
+This file conatains the processing part of the trial
+"""
+
+
+class TrialService:
+    """
+    In this class, we will process the data of the incoming trial,
+    and return the required information of the next trial
+    """
+
+    def get_trial_data(self, trial_no=None):
+        """
+        :param trial_no:
+        :return:
+        """
+        return
