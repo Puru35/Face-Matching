c781cfb2479437fd67a5a39b3d6597d0b4ac2b08
Rohan Rajesh P <rohan.rajesh@kaleyra.com>
2022-07-29 17:35:59 +0530

    Full functionality for BE has been implemented and tested. Working as expected from postman.

--- fm_project/service/trial_service.py
+++ fm_project/service/trial_service.py
@@ -20,7 +20,7 @@ class TrialService:
         self.correct_image = ""
         self.is_correct = 0
         self.trial_type = ""
-        self.total_score = ""
+        self.total_score = 0
         self.next_trial_no = self.next_trial_number()
 
     def next_trial_number(self):
@@ -40,7 +40,10 @@ class TrialService:
         return:
             next_trial_data: the required info for the next trial -> dict
         """
-        path = Path(__file__).parent / f"..{os.sep}..{os.sep}trial_images{os.sep}{self.next_trial_no}"
+        if self.trial_no == "T30":
+            return {"message": "Test completed"}
+        path = Path(
+            __file__).parent / f"..{os.sep}..{os.sep}trial_images{os.sep}{self.next_trial_no}"
         image_name_list = os.listdir(path)
         correct_image_name = ""
         for filename in image_name_list:
@@ -79,14 +82,21 @@ class TrialService:
         else:
             self.correct_image = f"{image_name[:-1]}.{extension}"
         self.get_trial_type()
-        self.save_trial_data()
+        if int(self.trial_no[1:]) and int(self.trial_no[1:]) % 10 == 0:
+            cumulative_score = self.add_cumulative_score()
+            self.save_trial_data(cumulative_score)
+        else:
+            self.save_trial_data()
+        if self.trial_no == "T30":
+            self.json_to_csv()
 
-    def save_trial_data(self):
+    def save_trial_data(self, cumulative_score=None):
         """
         This method saves the trial data to the respective file of the user
         """
-
-        json_filename = Path(__file__).parent /f"..{os.sep}..{os.sep}json_data{os.sep}{self.name}.json"
+        path = Path(
+            __file__).parent / f"..{os.sep}..{os.sep}json_data{os.sep}{self.name}.json"
+        json_filename = path
         trial_data = {
             'trial_no': self.trial_no,
             'trial_type': self.trial_type,
@@ -95,6 +105,8 @@ class TrialService:
             'is_correct': self.is_correct,
             'time_taken': self.time_taken
         }
+        if cumulative_score is not None:
+            trial_data['cumulative_score'] = cumulative_score
         try:
             with open(json_filename) as f:
                 data = f.read()
@@ -112,5 +124,45 @@ class TrialService:
         with open(json_filename, "w") as f:
             f.write(json.dumps(final_data))
 
+    def add_cumulative_score(self):
+        """
+        This method calculates the cumulative scores for the previous 10 trials
+        """
+        path = Path(
+            __file__).parent / f"..{os.sep}..{os.sep}json_data{os.sep}{self.name}.json"
+        json_filename = path
+        with open(json_filename) as f:
+            data = f.read()
+            if data:
+                data = json.loads(data)[f'{self.name}']
 
+        sum = 0
+        for i in range(len(data) - 10, len(data)):
+            sum += data[i].get('is_correct')
 
+        return sum
+
+    def json_to_csv(self):
+        import pandas as pd
+        path = Path(
+            __file__).parent / f"..{os.sep}..{os.sep}json_data{os.sep}{self.name}.json"
+        json_filename = path
+        with open(json_filename) as f:
+            data = f.read()
+            if data:
+                data = json.loads(data)[f'{self.name}']
+        for i in data:
+            if i.get("cumulative_score"):
+                self.total_score += int(i.get("cumulative_score"))
+        data.append({"cumulative_score": self.total_score, "time_taken": "Total Score"})
+
+        with open(json_filename, "w") as f:
+            f.write(json.dumps(data))
+
+        with open(json_filename) as f:
+            data = f.read()
+
+        dataframe = pd.read_json(data)
+        path = Path(
+            __file__).parent / f"..{os.sep}..{os.sep}trial_results{os.sep}{self.name}.csv"
+        dataframe.to_csv(path, index=False)
