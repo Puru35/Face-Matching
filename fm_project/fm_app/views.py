62b587798fbda686a52799832b9dfc866a39f7e0
Achu <achusimha98@gmail.com>
2022-08-04 22:46:46 +0530

    fm_app added

---
+++ fm_project/fm_app/views.py
@@ -0,0 +1,24 @@
+import json
+
+from django.views import View
+from django.http.response import JsonResponse
+
+from service import TrialService
+
+
+class Trial(View):
+    """
+    In this class, we will receive the data from the FE, process it,
+    and send back the data of the next trial. If it is the first trial (T1),
+    then we won't be receiving any data
+    """
+
+    def post(self, request):
+        trial_no = json.loads(request.body.decode("utf-8")).get("trial_no", None)
+        trial_service = TrialService(json.loads(request.body.decode("utf-8")), trial_no)
+        if trial_no:
+            trial_service.process_trial_data()
+        return JsonResponse(
+            trial_service.get_next_trial_data(),
+            status=200
+        )
